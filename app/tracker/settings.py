5a690adf16cb594d81aefb9c86635dcadb834d91
Cyno India <cyno@Cynos-MacBook-Air.local>
2023-01-04 16:34:15 +0530

    first commit

--- app/tracker/settings.py
+++ app/tracker/settings.py
@@ -117,11 +117,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -185,4 +189,6 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['https://api.cyno.co.in']
+# CSRF_TRUSTED_ORIGINS = ['https://api.cyno.co.in']
+
+
